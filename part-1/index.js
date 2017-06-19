87bcc6b560a8009d6ab6cd4662772d9df4b3a5c1
Ryan Greenberg <hawk1138@gmail.com>
2017-06-19 11:11:35 -0700

    Quick and dirty completion of all core specs for part-1 (phew)

---
+++ part-1/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Welcome to math-basic, try some routes! (We suggest /zero /add /subtract and /double/:num)')
+})
+
+app.get('/zero', function (req, res) {
+  res.send('0')
+})
+
+app.get('/add', function (req, res) {
+  res.send(String(Number(req.query.a) + Number(req.query.b)))
+})
+
+app.get('/subtract', function (req, res) {
+  res.send(String(Number(req.query.a) - Number(req.query.b)))
+})
+
+app.get('/double/:number', function(req, res) {
+  res.send(String(req.params.number * 2))
+
+})
+
+app.listen(3000, function () {
+  console.log('listening on port 3000!')
+})
