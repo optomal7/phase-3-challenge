1694a69f7a0fd8d3a789d8ee1098d463cd535435
Ryan Greenberg <hawk1138@gmail.com>
2017-08-01 01:04:07 -0700

    Mostly done, requires tests

--- part-2/database.js
+++ part-2/database.js
@@ -1,5 +1,20 @@
 const pgp = require('pg-promise')();
 
+const connectionString = 'postgres://localhost:5432/grocery_store';
+const db = pgp(connectionString);
+
 const Groceries = {
-  
+  productSection(section) {
+    return db.any('SELECT name AS "Product Name", section AS "Section" FROM items WHERE section = $1', [section]);
+  },
+
+  realShoppers() {
+    return db.any('SELECT shoppers.name, COUNT (orders.id) FROM orders, shoppers WHERE (shoppers.id = orders.shopper_id) GROUP BY shoppers.name');
+  },
+
+  shopperOrder(shopperID) {
+    return db.any('SELECT order_items.order_id, SUM(items.price) FROM order_items INNER JOIN items ON (order_items.item_id = items.id) INNER JOIN orders ON (order_items.order_id = orders.id) WHERE (orders.shopper_id = $1) GROUP BY (order_items.order_id)', [shopperID])
+  }
 }
+
+module.exports = Groceries;
